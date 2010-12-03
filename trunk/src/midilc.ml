2c6bfbabc0baa37748a82ed7d885c907b567318f
8enmann@gmail.com <8enmann@gmail.com@d8f64e84-db09-8dc1-1221-e74a79f3ae00>
2010-12-03 21:43:15 +0000

    Added toplevel for Ye to work on.

---
+++ trunk/src/midilc.ml
@@ -0,0 +1,20 @@
+type action = Ast | Interpret | Bytecode | Compile
+
+(** find out how to call java from inside ml? *)
+
+let _ =
+  let action = if Array.length Sys.argv > 1 then
+    List.assoc Sys.argv.(1) [ ("-a", Ast);
+			      ("-b", Bytecode);
+			      ("-c", Compile) ]
+  else Compile in
+  let lexbuf = Lexing.from_channel stdin in
+  let program = Parser.program Scanner.token lexbuf in
+  match action with
+    Ast -> let listing = Ast.string_of_program program
+           in print_string listing
+  | Bytecode -> let listing =
+      Bytecode.string_of_prog (Compile.translate program)
+    in print_endline listing
+  | Compile -> Execute.execute_prog (Compile.translate program)
+ 
