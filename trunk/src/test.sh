0106b5b5e4601a9e87326ae7fd73ae0d8cf8505b
8enmann@gmail.com <8enmann@gmail.com@d8f64e84-db09-8dc1-1221-e74a79f3ae00>
2010-12-14 20:41:00 +0000

    Deleted java files since they've been moved to /components. Added test script and executable jar file.

---
+++ trunk/src/test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+./midilc -c < tests/test-$1.m > test.csv
+
+echo "=============CSV=================="
+cat test.csv
+echo "=================================="
+
+java -jar CSV2MIDI.jar test.csv test.midi
+
+rm test.csv
+
+echo "========Converting MIDI==========="
+timidity test.midi --volume=250 -Ow1S -s 44100 -o test.wav
+echo "===========Playing MIDI==========="
+timidity test.midi
