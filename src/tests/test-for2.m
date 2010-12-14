778ef72d1f21449bd084088e38ab7b6a2f200a50
8enmann@gmail.com <8enmann@gmail.com@d8f64e84-db09-8dc1-1221-e74a79f3ae00>
2010-12-14 20:22:19 +0000

    New tests added

---
+++ src/tests/test-for2.m
@@ -0,0 +1,12 @@
+main() {
+  Note a; 
+  Sequence b;
+  Number i;
+  b = new_sequence();
+  a = Aq;
+  for (i = 0 ; i < 12 ; i = i + 1) {
+    b = b + a;
+    b = b + ((a as Chord) + (a .+ i));
+  }
+  play(b);
+}
