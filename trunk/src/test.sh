644575353d66bef1f7472629ce84d8553181fca9
8enmann@gmail.com <8enmann@gmail.com@d8f64e84-db09-8dc1-1221-e74a79f3ae00>
2010-12-15 08:13:27 +0000

    Modified makefile so you can just type "make test" to run all the tests. Also edited the test.sh script to make the gold standard of the correct filename format. Added a missing gold standard for for4.

--- trunk/src/test.sh
+++ trunk/src/test.sh
@@ -9,6 +9,8 @@ echo "=================================="
 
 java -jar CSV2MIDI.jar test.csv test.midi
 
+mv test.csv test-$1.out
+
 echo "========Converting MIDI==========="
 timidity test.midi --volume=250 -Ow1S -s 44100 -o test.wav
 echo "===========Playing MIDI==========="
