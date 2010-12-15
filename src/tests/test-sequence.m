8b59702390cdffac70a7339725a66e4d5805cbc4
Fredmaster2 <Fredmaster2@d8f64e84-db09-8dc1-1221-e74a79f3ae00>
2010-12-15 02:56:47 +0000

    Added gold standard output, removed interpret test from testall.sh, and wrote sequence test

---
+++ src/tests/test-sequence.m
@@ -0,0 +1,22 @@
+main() {
+  Note a;
+  Sequence b;
+  Sequence seq;
+  Note root;
+  
+  Chord chor;
+  Chord chor2;
+  Chord chor3;
+  b = new_sequence();
+  seq = new_sequence();
+  root = C4q;
+  chor = (root as Chord) + E4q + G4q;
+  b = b + chor;
+  root = F4q;
+  chor2 = (root as Chord) + A5q + C5q;
+  root = G4q;
+  chor3 = (root as Chord) + B5q + D5q;
+  seq = seq + chor2 + chor3;
+  seq = b + seq;
+  play(seq);
+}
