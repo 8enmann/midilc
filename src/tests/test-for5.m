903f3c07e354b861717794c841bd3436f107eab9
Akiva.Bamberger <Akiva.Bamberger@d8f64e84-db09-8dc1-1221-e74a79f3ae00>
2010-12-16 23:11:24 +0000

    Added tests for recursion and added comments for break/continue.
    Allowed using types in declarations.

--- src/tests/test-for5.m
+++ src/tests/test-for5.m
@@ -1,15 +1,23 @@
+/**
+ *  This code demonstrates continue and break.
+ *
+ *  The output is shown to not include C#, as we continue on i==4.
+ *  In addition, the length of output is limited by the break command.
+ */
+
 main(){
   Note d;
   Sequence c;
   Number i;
-  c = new_sequence();
-  c = c + A;
+
+  c= new_sequence();
   d = A;
-  for(i = 0; i < 20; i=i+1){
+
+  for(i = 0; i < 200; i=i+1){
     if(i==4){
         continue;
     } else {
-        c = c + (d .+ i);
+        c = c + (A .+ i);
     }
     if(i==12){
         break;
